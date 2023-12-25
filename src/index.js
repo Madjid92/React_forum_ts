22444c3a34aace8139c827004dd9839b66745176
djerada-madjid <djerada.madjid@gmail.com>
2023-12-25 16:59:17 +0100

    Handle font on webpack config

--- src/index.js
+++ src/index.js
@@ -1,12 +1,20 @@
 import  _  from 'lodash';
 import "../assets/styles.css";
-import img from "../assets/robbie-lens.png"
+import printMe from './print';
+import img from "../assets/robbie-lens.png";
 
 function component() {
     const element = document.createElement('div');
+    const btn = document.createElement('button');
+    
 
     // Lodash, currently included via a script, is required for this line to work
     element.innerHTML = _.join(['Hello', 'webpack'], ' ');
+
+    btn.innerHTML = 'Click me and check the console!';
+    btn.onclick = printMe;
+
+    element.appendChild(btn);
   
     return element;
   }
