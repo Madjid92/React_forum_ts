e3b483d695422b4150f8fbc1235c634da5fb8cf5
djerada-madjid <djerada.madjid@gmail.com>
2023-12-25 16:18:06 +0100

    handle assets in webpack config

--- src/index.js
+++ src/index.js
@@ -1,7 +1,10 @@
-import _ from 'lodash';
+import  _  from 'lodash';
+import "../assets/styles.css";
+import img from "../assets/robbie-lens.png"
+
 function component() {
     const element = document.createElement('div');
-  
+
     // Lodash, currently included via a script, is required for this line to work
     element.innerHTML = _.join(['Hello', 'webpack'], ' ');
   
@@ -9,3 +12,6 @@ function component() {
   }
   
   document.body.appendChild(component());
+  const imgNode = document.createElement('img');
+  imgNode.setAttribute('src', img );
+  document.body.appendChild(imgNode)
--- webpack.config.js
+++ webpack.config.js
@@ -6,6 +6,18 @@ module.exports = {
   entry: {
     index: './src/index.js',
   },
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader'],
+      },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        type: 'asset/resource',
+      },
+    ],
+  },
   plugins: [
     new HtmlWebpackPlugin({
      title: 'Development',
