534bbc401798e9a063da235fda1a26db41a954c6
djerada-madjid <djerada.madjid@gmail.com>
2023-12-28 17:35:38 +0100

    add react eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,9 +1,14 @@
-// .eslintrc.js example
+
 module.exports = {
     ignorePatterns: ["node_modules/**/*", "dist/**/*"],
     env: {
         browser: true,
         es2021: true,
+    },
+    settings:{
+      react : {
+        version : "detect"
+      }
     },
     parserOptions : {
       ecmaVersion: "latest",
@@ -24,7 +29,24 @@ module.exports = {
         files: ["src/**/*"],
         parser: '@typescript-eslint/parser',
         plugins: ['@typescript-eslint'],
-        extends: ['plugin:@typescript-eslint/recommended'],
+        extends: [
+          "plugin:@typescript-eslint/recommended", 
+          "plugin:react/recommended",
+          "plugin:react-hooks/recommended",
+          "plugin:jsx-a11y/recommended",
+          "plugin:import/typescript",
+          "plugin:import/errors",
+          "plugin:import/warnings",
+        ],
+        /*parserOptions: {
+          ecmaFeatures: {
+            "jsx": true
+          }
+        },*/
+        rules: {
+          "react/jsx-uses-react": "off",
+          "react/react-in-jsx-scope": "off"
+         }
       }
     ],
     extends: ["eslint:recommended"],
--- src/index.tsx
+++ src/index.tsx
@@ -4,6 +4,7 @@ import ReactDOM from 'react-dom/client';
 const root = ReactDOM.createRoot(document.getElementById('root') as Element)
 
 
+
 root.render(
     <div> hello majid</div>
 );
